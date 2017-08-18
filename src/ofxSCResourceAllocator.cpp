9f032c47b62e0bc572e6aa5855f83e101ff366d4
Atsushi Tadokoro <tadokoro@gmail.com>
2017-08-18 09:02:10 +0900

    fix resorce allocation error

--- src/ofxSCResourceAllocator.cpp
+++ src/ofxSCResourceAllocator.cpp
@@ -19,47 +19,56 @@ ofxSCResourceAllocator::ofxSCResourceAllocator(int capacity)
 	this->pos = 0;
 	
 	this->resources.reserve(capacity);
-	for (int i = 0; i < capacity; i++)
-		this->resources[i] = NULL;
-						
+	if (capacity <= this->resources.size()){
+		for (int i = 0; i < capacity; i++)
+			this->resources[i] = NULL;
+	}
+							
 	this->free_lists.reserve(32);
-	for (int i = 0; i < 32; i++)
-		this->free_lists[i] = NULL;
+	if (this->free_lists.size() >= 32){
+		for (int i = 0; i < 32; i++)
+			this->free_lists[i] = NULL;
+	}
 }
 
 
 int ofxSCResourceAllocator::alloc (int resource_size)
 {
-	ofxSCResource *res = free_lists[resource_size];
-	
-	// Resource of this size has previously been freed.
-	if (res != NULL)
-	{
-		free_lists[resource_size] = res->next;
-		return res->address;
+	ofxSCResource *res;
+	if (resource_size < free_lists.size()) {
+		res = free_lists[resource_size];
+
+		// Resource of this size has previously been freed.
+		if (res != NULL)
+		{
+			free_lists[resource_size] = res->next;
+			return res->address;
+		}
+
+		// Not got previously freed resource but some have yet to be allocated.
+		if ((pos + resource_size) <= capacity)
+		{
+			int address;
+			resources[pos] = new ofxSCResource(pos, resource_size);
+			address = pos;
+			pos += resource_size;
+			return address;
+		}
 	}
-	
-	// Not got previously freed resource but some have yet to be allocated.
-	if ((pos + resource_size) <= capacity)
-	{
-		int address;
-		resources[pos] = new ofxSCResource(pos, resource_size);
-		address = pos;
-		pos += resource_size;
-		return address;
-	}
-	
 	return -1;
 }
 
 void ofxSCResourceAllocator::free (int address)
 {
-	ofxSCResource *res = resources[address];
-	
-	if (res != NULL)
-	{
-		res->next = free_lists[res->size];
-		free_lists[res->size] = res;
-		// should we set resources[address] = null?
+	ofxSCResource *res;
+	if (address < resources.size()) {
+		res = resources[address];
+
+		if (res != NULL)
+		{
+			res->next = free_lists[res->size];
+			free_lists[res->size] = res;
+			// should we set resources[address] = null?
+		}
 	}
 }
